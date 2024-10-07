214b3578ec48a38aec8f4907877f56bdfd7f0d12
dattateja99 <anakala@usc.edu>
2024-10-06 18:20:19 -0700

    Scripts
    Scripts to run the scrapper

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+
+# Reddit API credentials
+REDDIT_CLIENT_ID = "Kl0y3MddL4xyViAwOp2Beg"
+REDDIT_CLIENT_SECRET = "mqq_y9lIeN759fzn_mClYb4eZSN9wg"
+REDDIT_USER_AGENT = "my-reddit-scraper:v1.0 (by /u/Active-Parsnip3646)"
+
+# MongoDB settings
+MONGO_URI = "mongodb://localhost:27017/"
+MONGO_DB_NAME = "reddit_db"
+MONGO_COLLECTION_NAME = "posts"
+
+# Tesseract settings (optional)
+TESSERACT_CMD = "/usr/local/bin/tesseract"
