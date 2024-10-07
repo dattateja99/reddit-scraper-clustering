3bb461b0a0c14bcd42892a1041a20b9ebbdd89e9
dattateja99 <anakala@usc.edu>
2024-10-06 18:26:33 -0700

    removed secret

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # settings.py
 
 # Reddit API credentials
-REDDIT_CLIENT_ID = "Kl0y3MddL4xyViAwOp2Beg"
-REDDIT_CLIENT_SECRET = "mqq_y9lIeN759fzn_mClYb4eZSN9wg"
-REDDIT_USER_AGENT = "my-reddit-scraper:v1.0 (by /u/Active-Parsnip3646)"
+REDDIT_CLIENT_ID = "****"
+REDDIT_CLIENT_SECRET = "***"
+REDDIT_USER_AGENT = "***"
 
 # MongoDB settings
 MONGO_URI = "mongodb://localhost:27017/"
